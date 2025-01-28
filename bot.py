6beed8f052dfc3a7a95b650fc8c11c2733ab3825
Rahat4089 <83128928+Rahat4089@users.noreply.github.com>
2025-01-28 13:12:23 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import utils as pyroutils
+pyroutils.MIN_CHANNEL_ID = -1002457905787
+from config import API_ID, API_HASH, BOT_TOKEN, ADMIN
+
+# Bot configuration
+API_ID = API_ID
+API_HASH = API_HASH
+BOT_TOKEN = BOT_TOKEN
+plugins = dict(root="plugins")
+# Initialize the bot
+app = Client("anime_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=plugins)
+
+
+# Start the bot
+if __name__ == "__main__":
+    print("Bot is starting...")
+    app.run()
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+API_ID = 24720817
+API_HASH = "43669876f7dbd754e157c69c89ebf3eb"
+BOT_TOKEN = "7947822923:AAHN7swdJsj0w59pLMDFIahQrlgsWpLHUb4"
+#BOT_TOKEN = "6794711820:AAFC2Dn3xP08K75d5uxcpUZJU_ZNcY2SUu0"
+# MongoDB setup
+MONGO_URL = "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority&appName=animepahe"
+DB_NAME = "animepahe"
+
+DOWNLOAD_DIR = "./downloads"
+#ADMIN INFO
+ADMIN = 1235222889
+
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg https://graph.org/file/b55f0baaa7a6fde7c5682.jpg https://graph.org/file/3298ca8910c82f33418a8.jpg https://graph.org/file/4e4935469a7214c734721.jpg https://graph.org/file/fa5f2b241fe77beff8ba0.jpg https://graph.org/file/bcb9969f78ab4a47a483c.jpg https://graph.org/file/a9af161696d82f17b8888.jpg https://graph.org/file/d23b00650c00ce4d9a467.jpg https://graph.org/file/4dc0b3dfaad61fbcf0a49.jpg https://graph.org/file/7088315e9b0b6a2fa7118.jpg https://graph.org/file/9c9911d06e5f2316febb9.jpg https://graph.org/file/d2ee185180469cfd28071.jpg https://graph.org/file/dea04b2d615406aeb0181.jpg https://graph.org/file/98b63d3bb84984a68cc76.jpg").split()
